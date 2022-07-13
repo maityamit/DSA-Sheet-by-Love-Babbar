0e2e07f65dd9704805c57fd6fcddbcc1d51effb8
Amit Maity <maityamit308@gmail.com>
2022-07-13 16:24:06 +0530

    Create Find the "Kth" max and min element of an array .cpp

---
+++ Array/Find the "Kth" max and min element of an array .cpp
@@ -0,0 +1,14 @@
+int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        priority_queue<int> pq;
+        for(int i=0;i<k;i++){
+            pq.push(arr[i]);
+        }
+        for(int i=k;i<=r;i++){
+            if(pq.top()>arr[i]){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        return pq.top();
+    }
